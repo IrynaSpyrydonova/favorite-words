e5f7f152d91d741ec5971a892280a6bce7fe2df5
Iryna Spyrydonova <Iryna059@gmail.com>
2020-07-14 19:46:46 +0200

    Wrote the logic for exist function
    Closes gh-7

--- scripts/user-actions/exists.js
+++ scripts/user-actions/exists.js
@@ -3,18 +3,19 @@ console.log('--- loading: exists.js');
 
 const existsHandler = () => {
   debugger;
-  const userInput = prompt('');
-  if (_) {
+  const userInput = prompt('find if a word exists:');
+  if (userInput === null) {
     return;
   }
 
-  const equalsUserInput = (entry) => {
-    return _;
-  }
-  const itExists = words._(_);
+  const equalsUserInput = words.find((entry) => 
+  entry === userInput
+);
+
+  const itExists = words.includes(equalsUserInput);
   if (itExists) {
-    alert(``);
+    alert(`"${userInput}" exists`);
   } else {
-    alert(``);
+    alert(`"${userInput}" does not exist`);
   }
 };
