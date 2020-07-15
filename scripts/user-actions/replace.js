b08385be5ec5c73531e80c84154200994f2b7715
fatih <fatihkarakus@hotmail.com>
2020-07-15 13:45:26 +0200

    Updated replace.js

--- scripts/user-actions/replace.js
+++ scripts/user-actions/replace.js
@@ -1,29 +1,33 @@
-'use strict';
-console.log('--- loading: replace.js');
+"use strict";
+console.log("--- loading: replace.js");
 
 const replaceHandler = () => {
   debugger;
-  const userInput1 = prompt('enter the word to replace:');
+  const userInput1 = prompt("enter the word to replace:");
   if (userInput1 === null) {
     return;
   }
-
+  /*
   const entryIsUserInput = (entry) => {
     return _;
   };
-  const inputExists = words._(_);
-  if (_) {
-    alert(``);
+*/
+  const inputExists = words.includes(userInput1);
+  console.log(inputExists);
+  if (!inputExists) {
+    alert(`"${userInput1}" does not exist`);
     return;
   }
 
-  const userInput2 = prompt(``);
-  if (_) {
+  const userInput2 = prompt(`enter a word to replace "${userInput1}":`);
+  if (userInput2 === null) {
     return;
   }
 
-  const replaceWithInput2 = (entry) => { };
-  words = words._(replaceWithInput2);
+  const replaceWithInput2 = (entry) =>
+    entry === userInput1 ? userInput2 : entry;
 
-  displayHandler(``);
+  words = words.map(replaceWithInput2);
+
+  displayHandler(`"${userInput1}" has been replaced by "${userInput2}":`);
 };
